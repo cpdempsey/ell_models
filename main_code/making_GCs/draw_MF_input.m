6cbb9b6c782aa58e388d6e66dcc208fcb72c22a6
Salomon <szm2106@columbia.edu>
2017-12-27 13:51:19 -0500

    avoiding redrawing from same mf trial

--- main_code/making_GCs/draw_MF_input.m
+++ main_code/making_GCs/draw_MF_input.m
@@ -1,6 +1,23 @@
 function input = draw_MF_input(mossyraster)
 
-trnum = randperm(size(mossyraster,1));
-input = mossyraster(trnum(1),:);
+trials_to_choose_from = setdiff(1:size(mossyraster,1),mf_trials);
+
+if isempty(trials_to_choose_from)
+    mf_trials_sub = mf_trials;
+end
+
+while isempty(trials_to_choose_from)
+    [~,mf_idx] = unique(mf_trials_sub);
+    mf_trials_sub(mf_idx) = 0;
+    trials_to_choose_from = setdiff(1:size(mossyraster,1),mf_trials_sub');
+end
+
+trnum = randi(length(trials_to_choose_from));
+trnum = trials_to_choose_from(trnum);
+input = mossyraster(trnum,:);
+
+% trnum = randi(size(mossyraster,1));
+% input = mossyraster(trnum,:);
 
 end
+
