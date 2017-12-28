af6ce810b5e56bfac6c7ad76c3d1048f866d2bad
Salomon <szm2106@columbia.edu>
2017-12-28 13:03:42 -0500

    fix a bug in thresh adjustment

--- main_code/making_GCs/tonic_thr_adjust.m
+++ main_code/making_GCs/tonic_thr_adjust.m
@@ -7,21 +7,22 @@ delta_baseline = 0;
 dt=GC_model.dt;
 
 for i=1:(length(GC_model.Ws)/2)
-    if(sum(MF_indices.tonic==GC_model.mf_input(i)))
+    if  ismember(GC_model.mf_input(i),MF_indices.tonic)
         trnum = randi(size(rspstore{GC_model.mf_input(i)},1));
         input = rspstore{GC_model.mf_input(i)}(trnum,:);
         sptrain     = find(input);
         meanISI     = mean(sptrain(2:end)-sptrain(1:end-1));  %in dt's
-        
+        if isnan(meanISI)
+            continue
+        end
         tonic_sc    = 1/(meanISI*dt); %this is the height of the equivalent rectangle of the spike train in sptrain
         
         fast_change = sum(spikes.kernel_fast*dt)*tonic_sc*GC_model.Ws(i);
         slow_change = sum(spikes.kernel_slow*dt)*tonic_sc*GC_model.Ws(i+(length(GC_model.Ws)/2));
         
         delta_baseline = delta_baseline + fast_change + slow_change;
-    end
-    
-    if(sum(MF_indices.pause==GC_model.mf_input(i)))
+          
+    elseif  ismember(GC_model.mf_input(i),MF_indices.pause)
         trnum = randi(size(rspstore{GC_model.mf_input(i)},1));
         input = rspstore{GC_model.mf_input(i)}(trnum,:);
         sptrain     = find(input);
@@ -29,7 +30,10 @@ for i=1:(length(GC_model.Ws)/2)
         [~,largest_interval] = max(ISIs);
         ISIs = ISIs(largest_interval+1:end);
         meanISI     = mean(ISIs);  %in dt's
-        
+        if isnan(meanISI)
+            continue
+        end
+
         pause_sc    = 1/(meanISI*dt); %this is the height of the equivalent rectangle of the spike train in sptrain
         
         fast_change = sum(spikes.kernel_fast*dt)*pause_sc*GC_model.Ws(i);
